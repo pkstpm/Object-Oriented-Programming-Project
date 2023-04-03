2cdb39489e226074035331ddb074598e4d0aa152
whiskaz112 <wisapat2546@gmail.com>
2023-04-03 14:52:13 +0700

    Update Review.py

--- Review.py
+++ Review.py
@@ -1,4 +1,20 @@
 class Review:
     def __init__(self, rating, comment):
-        self.rating = rating
-        self.comment = comment
+        self.__rating = rating
+        self.__comment = comment
+
+    @property
+    def rating(self):
+        return self.__rating
+    @rating.setter
+    def set_rating(self,rating):
+        self.__rating = rating
+        return self.__rating
+    
+    @property
+    def comment(self):
+        return self.__comment
+    @comment.setter
+    def set_comment(self,comment):
+        self.__comment = comment
+        return self.__comment
